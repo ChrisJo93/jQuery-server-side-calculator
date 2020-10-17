204763736dc1f6cd902e5b4d289a7782bc40c34d
Chris Johnson <Johnny.C.Alexander@gmail.com>
2020-10-17 00:47:02 -0500

    server boilerplate up. packages and depends installed

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.listen(PORT, function () {
+  console.log('listening on port', PORT);
+});
+
+console.log('working cap');
