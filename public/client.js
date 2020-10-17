2efc24508a0de79ac91153b065113127118cd493
Chris Johnson <Johnny.C.Alexander@gmail.com>
2020-10-17 14:14:22 -0500

    captured input fields with key value pairs matching operations. Beginning post for object array

--- public/client.js
+++ public/client.js
@@ -5,6 +5,37 @@ function onReady() {
   $('.buttonSubtract').on('click', subtraction);
   $('.buttonMultiply').on('click', multiplication);
   $('.buttonDivide').on('click', division);
-  $('.buttonEquals').on('click', equals);
+  $('.buttonEquals').on('click', submitEquals);
   $('buttonClear').on('click', clear);
+}
+
+function submitEquals() {
+  let firstNumber = $('.firstInputField').val();
+  let secondNumber = $('.secondInputField').val();
+  const numberInputs = {
+    firstNumber,
+    secondNumber,
+    add: false,
+    subtract: false,
+    division: false,
+    muliply: false,
+  };
+  console.log(firstNumber, secondNumber);
+}
+
+function addition() {
+  console.log('add works');
+}
+function subtraction() {
+  console.log('sub works');
+}
+function multiplication() {
+  console.log('mulitply works');
+}
+function division() {
+  console.log('division works');
+}
+
+function clear() {
+  console.log('clear works');
 }
