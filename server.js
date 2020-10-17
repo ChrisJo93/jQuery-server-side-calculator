04f150eeb8ec7b00c86bb7b4a0f8dcbe5e67e69d
Chris Johnson <Johnny.C.Alexander@gmail.com>
2020-10-17 01:28:24 -0500

    server test in postman clears. Console log clears.

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
+app.get('/test', (req, res) => {
+  res.send("fuckin' about");
+});
+
+app.post('/test', (req, res) => {
+  console.log("still fuckin' about");
+  res.sendStatus(200);
+});
+
 app.listen(PORT, function () {
   console.log('listening on port', PORT);
 });
